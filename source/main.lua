09d9eca76805e66ffe7ffa2350fc29febeb04bef
TheVonKanar <thibault.eynard@pm.me>
2026-02-01 15:13:57 +0100

    egg

--- source/main.lua
+++ source/main.lua
@@ -9,15 +9,7 @@ import "CoreLibs/timer"
 import "masks"
 import "menu"
 
-local letters = { "A", "Z", "E", "R", "T", "Y", "U", "I", "O", "P", "Q", "S", "D", "F", "G", "H", "J", "K", "L", "M", "W",
-   "X", "C", "V", "B", "N" }
-
-local banks = {
-   json.decodeFile("words/6-letter-words.json"),
-   json.decodeFile("words/7-letter-words.json"),
-   json.decodeFile("words/8-letter-words.json"),
-   json.decodeFile("words/9-letter-words.json")
-}
+local letters = { "E", "G", "G" }
 
 ---- LOCALS ----
 
@@ -25,58 +17,41 @@ local pd <const> = playdate
 local gfx <const> = playdate.graphics
 local geo <const> = playdate.geometry
 
-local crankMul = 0.1
+local crankMul = 0.2
 
 local rawGridY = 0
 local gridY = 0
 local rows = Menu
 local cellWidth = 20
 local cellHeight = 20
-local maxRows = 64
-local focusedRow1
-local focusedRow2
+local maxRows = 16
 
 ---@class _Polygon
 local maskPolygon
 local mask
-local maskX = cellWidth * 3
-local maskY
-local maskIndex = 1
+local maskX = cellWidth * 5
+local maskY = cellHeight * 4
+local maskIndex = 6
 
-local timerDuration = 5000
-local score = 0
+local sample = pd.sound.sampleplayer.new("egg.wav")
+
+local goLeftTimer
+local goRightTimer
+local goUpTimer
+local goDownTimer
 
 ---- FUNCTIONS ----
 
 local function createRow()
-   local bankIndex = math.random(#banks)
-   local bank = banks[math.random(bankIndex)]
-   local word = string.upper(bank[math.random(#bank)])
-
    local row = { cells = {} }
-
-   local wordSize = #word
-   local gapSize = 10 - wordSize
-   local gapStart = math.random(wordSize)
-   for i = 1, #word do
-      row.cells[i] = string.sub(word, i, i)
-   end
-
-   for i = gapStart, gapStart + gapSize - 1 do
+   for i = 1, 10 do
       table.insert(row.cells, i, letters[math.random(#letters)])
    end
-
-   row.gapSize = gapSize
-   row.gapStart = gapStart
-   row.wordSize = wordSize
-   row.word = word
    return row
 end
 
 local function pickMask(i)
    mask = Masks[i]
-
-   maskY = cellHeight * 6 - mask.size[2] * cellHeight
 
    local points = {}
    for _, p in ipairs(mask.polygon) do
@@ -87,50 +62,27 @@ local function pickMask(i)
    maskPolygon = geo.polygon.new(table.unpack(points))
 end
 
-local function tryValidateRow(y)
-   if y < 1 or y > #rows then
-      return
-   end
-   local row = rows[y]
-   local gapStart = nil
-   local gapSize = 0
-   for x = 1, #row.cells do
-      local point = geo.point.new(
-         (x - 1) * cellWidth,
-         gridY + ((y - 1) * cellHeight) + 2
-      ):offsetBy(cellWidth / 2, cellHeight / 2)
+local function validate()
+   for y, row in ipairs(rows) do
+      for x = 1, #row.cells do
+         local point = geo.point.new(
+            (x - 1) * cellWidth,
+            gridY + ((y - 1) * cellHeight) + 2
+         ):offsetBy(cellWidth / 2, cellHeight / 2)
 
-      if maskPolygon:containsPoint(point) then
-         gapStart = gapStart or x
-         gapSize = gapSize + 1
+         if maskPolygon:containsPoint(point) then
+            rows[y].cells[x] = " "
+         end
       end
    end
 
-   if row.gapStart == gapStart and row.gapSize == gapSize then
-      rows[y].isSolved = true
-      rows[y].cells = { "G", "G", "}", "+", "1", "0", "0", "{", "G", "G", }
+   local new = maskIndex
+   while new == maskIndex do
+      new = math.random(#Masks)
    end
-end
-
-local function onTimerEnd()
-   pd.resetElapsedTime()
-
-   if #rows == 0 then
-      pd.restart()
-      return
-   end
-
-   if rows[1].isMenu then
-      score = score + 0
-   elseif rows[1].isSolved then
-      score = score + 100
-   else
-      score = score - 50
-   end
-
-   table.remove(rows, 1)
-   rawGridY = rawGridY + cellHeight
-   gridY = gridY + cellHeight
+   maskIndex = new
+   pickMask(maskIndex)
+   sample:play()
 end
 
 ---- INIT ----
@@ -140,10 +92,14 @@ pd.display.setScale(2)
 gfx.setLineWidth(1)
 gfx.setBackgroundColor(gfx.kColorBlack)
 
-pickMask(maskIndex)
+for i = 1, maxRows do
+   table.insert(rows, createRow())
+end
 
-local timer = pd.timer.new(timerDuration, onTimerEnd)
-timer.repeats = true
+table.insert(rows, { cells = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", }, isMenu = true })
+table.insert(rows, { cells = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", }, isMenu = true })
+
+pickMask(maskIndex)
 
 ---- UPDATE LOOP ----
 
@@ -156,83 +112,18 @@ function pd.update()
    -- Crank (grid scrolling)
    local change, acceleratedChange = playdate.getCrankChange()
    rawGridY = rawGridY - change * crankMul
+   rawGridY = math.min(rawGridY, 0)
+   rawGridY = math.max(rawGridY, (#rows - 6 - 2) * -cellHeight)
    local oldGridY = gridY
    gridY = math.floor((rawGridY / cellHeight) + 0.5) * cellHeight
 
-   if focusedRow1 and focusedRow1 > 0 and focusedRow1 <= #rows then
-      rows[focusedRow1].isFocused = false
-   end
-   focusedRow1 = math.floor((cellHeight * 6 - gridY) / cellHeight)
-   if focusedRow1 and focusedRow1 > 0 and focusedRow1 <= #rows then
-      rows[focusedRow1].isFocused = true
-   end
-
-   if focusedRow2 and focusedRow2 > 0 and focusedRow2 <= #rows then
-      rows[focusedRow2].isFocused = false
-   end
-   if mask.size[2] > 1 then
-      focusedRow2 = math.floor((cellHeight * 5 - gridY) / cellHeight)
-   else
-      focusedRow2 = -1
-   end
-   if focusedRow2 and focusedRow2 > 0 and focusedRow2 <= #rows then
-      rows[focusedRow2].isFocused = true
-   end
-
-   -- Left / Right Arrows (mask horizontal movement)
-   local maskBounds = maskPolygon:getBoundsRect()
-   if pd.buttonJustPressed(pd.kButtonLeft) and
-       maskBounds.right > cellWidth then
-      maskPolygon:translate(-cellWidth, 0)
-      maskX = maskX - cellWidth
-   end
-
-   if pd.buttonJustPressed(pd.kButtonRight) and
-       maskBounds.left < 200 - cellWidth then
-      maskPolygon:translate(cellWidth, 0)
-      maskX = maskX + cellWidth
-   end
-
-   -- Up / Down Arrows (change mask)
-   if pd.buttonJustPressed(pd.kButtonUp) then
-      maskIndex = maskIndex - 1
-      if maskIndex < 1 then
-         maskIndex = #Masks
-      end
-      pickMask(maskIndex)
-   end
-
-   if pd.buttonJustPressed(pd.kButtonDown) then
-      maskIndex = maskIndex + 1
-      if maskIndex > #Masks then
-         maskIndex = 1
-      end
-      pickMask(maskIndex)
-   end
-
-   -- A / B buttons (validate)
-   if pd.buttonJustPressed(pd.kButtonA) or pd.buttonJustPressed(pd.kButtonB) then
-      tryValidateRow(focusedRow1)
-      tryValidateRow(focusedRow2)
-   end
-
-   --- GRID MANAGEMENT ---
-   if gridY < oldGridY then
-      local delta = math.ceil((oldGridY - gridY) / cellHeight)
-      for i = 1, delta do
-         table.insert(rows, createRow())
-      end
-
-      if #rows > maxRows then
-         for i = 1, #rows - maxRows do
-            table.remove(rows, 1)
-            rawGridY = rawGridY + cellHeight
-            gridY = gridY + cellHeight
-         end
-      end
-   end
+   if goLeftTimer and not pd.buttonIsPressed(pd.kButtonLeft) then goLeftTimer:pause() end
+   if goRightTimer and not pd.buttonIsPressed(pd.kButtonRight) then goRightTimer:pause() end
+   if goUpTimer and not pd.buttonIsPressed(pd.kButtonUp) then goUpTimer:pause() end
+   if goDownTimer and not pd.buttonIsPressed(pd.kButtonDown) then goDownTimer:pause() end
 
    --- DRAW GRID ---
+   local score = 0
    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
    for y, row in ipairs(rows) do
       for x = 1, #row.cells do
@@ -243,7 +134,76 @@ function pd.update()
          local center = point:offsetBy(cellWidth / 2, cellHeight / 2)
          if not maskPolygon:containsPoint(center) then
             local text = row.cells[x]
-            if row.isMenu or row.isSolved or row.isFocused then
+            local isSolved = false
+
+            if text == "E" and
+                x < 9 and
+                row.cells[x + 1] == "G" and
+                row.cells[x + 2] == "G" and
+                (x == 1 or row.cells[x - 1] == " ") and
+                (x == 8 or row.cells[x + 3] == " ")
+            then
+               isSolved = true
+               score = score + 1
+            end
+
+            if text == "G" and
+                x > 1 and
+                x < 10 and
+                row.cells[x - 1] == "E" and
+                row.cells[x + 1] == "G" and
+                (x == 2 or row.cells[x - 2] == " ") and
+                (x == 9 or row.cells[x + 2] == " ")
+            then
+               isSolved = true
+               score = score + 1
+            end
+
+            if text == "G" and
+                x > 2 and
+                row.cells[x - 1] == "G" and
+                row.cells[x - 2] == "E" and
+                (x == 3 or row.cells[x - 3] == " ") and
+                (x == 10 or row.cells[x + 1] == " ")
+            then
+               isSolved = true
+               score = score + 1
+            end
+
+            if text == "E" and
+                y > 1 and y < #rows - 3 and
+                rows[y - 1].cells[x] == " " and
+                rows[y + 1].cells[x] == "G" and
+                rows[y + 2].cells[x] == "G" and
+                rows[y + 3].cells[x] == " "
+            then
+               isSolved = true
+               score = score + 1
+            end
+
+            if text == "G" and
+                y > 2 and y < #rows - 2 and
+                rows[y - 2].cells[x] == " " and
+                rows[y - 1].cells[x] == "E" and
+                rows[y + 1].cells[x] == "G" and
+                rows[y + 2].cells[x] == " "
+            then
+               isSolved = true
+               score = score + 1
+            end
+
+            if text == "G" and
+                y > 3 and y < #rows - 1 and
+                rows[y - 3].cells[x] == " " and
+                rows[y - 2].cells[x] == "E" and
+                rows[y - 1].cells[x] == "G" and
+                rows[y + 1].cells[x] == " "
+            then
+               isSolved = true
+               score = score + 1
+            end
+
+            if row.isMenu or isSolved then
                text = "*" .. text .. "*"
             end
 
@@ -265,25 +225,86 @@ function pd.update()
    gfx.setColor(gfx.kColorWhite)
    gfx.drawPolygon(maskPolygon)
 
-   --- DRAW TIMER ---
-   local elapsed = pd.getElapsedTime()
-   local ratio = math.min(math.max((elapsed * 1000) / timerDuration, 0), 1)
+   -- --- DRAW SCORE ---
    gfx.setColor(gfx.kColorWhite)
-   gfx.fillRect(0, 0, 200, 20)
-
+   gfx.fillRect(0, 0, 40, 20)
    gfx.setColor(gfx.kColorBlack)
-   gfx.fillRect(52, 2, 146 * (1 - ratio), 16)
-   gfx.drawRect(52, 2, 146, 16)
-
-   --- DRAW SCORE ---
-   gfx.setColor(gfx.kColorBlack)
+   gfx.fillCircleAtPoint(2, 2, 1)
+   gfx.fillCircleAtPoint(38, 2, 1)
+   gfx.fillCircleAtPoint(2, 18, 1)
+   gfx.fillCircleAtPoint(38, 18, 1)
    gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
-   local scoreText = ""
-   if score > 0 then scoreText = "+" end
-   scoreText = scoreText .. tostring(score)
-   gfx.drawTextInRect("*" .. scoreText .. "*", 2, 2, 48, 20, nil, nil, kTextAlignment.center)
+   gfx.drawTextInRect("*" .. tostring(math.ceil(score / 3)) .. "*", 2, 2, 36, 20, nil, nil, kTextAlignment.center)
+end
 
-   --- DEBUG ---
-   -- Uncomment this to show fps in the top left corner.
-   -- gfx.drawText(tostring(pd.getFPS()), 4, 4)
+local function goLeft()
+   local maskBounds = maskPolygon:getBoundsRect()
+   if maskBounds.left > 0 then
+      maskPolygon:translate(-cellWidth, 0)
+      maskX = maskX - cellWidth
+   end
+end
+
+local function goRight()
+   local maskBounds = maskPolygon:getBoundsRect()
+   if maskBounds.right < 200 then
+      maskPolygon:translate(cellWidth, 0)
+      maskX = maskX + cellWidth
+   end
+end
+
+local function goUp()
+   local maskBounds = maskPolygon:getBoundsRect()
+   if maskBounds.top > 0 then
+      maskPolygon:translate(0, -cellHeight)
+      maskY = maskY - cellHeight
+   end
+end
+
+local function goDown()
+   local maskBounds = maskPolygon:getBoundsRect()
+   if maskBounds.bottom < 120 then
+      maskPolygon:translate(0, cellHeight)
+      maskY = maskY + cellHeight
+   end
+end
+
+function pd.leftButtonDown()
+   goLeftTimer = pd.timer.keyRepeatTimer(goLeft)
+end
+
+function pd.leftButtonUp()
+   goLeftTimer:pause()
+end
+
+function pd.rightButtonDown()
+   goRightTimer = pd.timer.keyRepeatTimer(goRight)
+end
+
+function pd.rightButtonUp()
+   goRightTimer:pause()
+end
+
+function pd.upButtonDown()
+   goUpTimer = pd.timer.keyRepeatTimer(goUp)
+end
+
+function pd.upButtonUp()
+   goUpTimer:pause()
+end
+
+function pd.downButtonDown()
+   goDownTimer = pd.timer.keyRepeatTimer(goDown)
+end
+
+function pd.downButtonUp()
+   goDownTimer:pause()
+end
+
+function pd.AButtonDown()
+   validate()
+end
+
+function pd.BButtonDown()
+   validate()
 end
